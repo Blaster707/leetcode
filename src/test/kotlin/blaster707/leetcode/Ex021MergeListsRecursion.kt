78a92ec394c35ae87526bf7a01e79ce3ef971561
Lucas Ferguson <blaster707dc@gmail.com>
2022-10-02 22:32:59 -0500

    Added Ex021 using Recursion

---
+++ src/test/kotlin/blaster707/leetcode/Ex021MergeListsRecursion.kt
@@ -0,0 +1,55 @@
+package blaster707.leetcode
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class Ex021MergeListsRecursion {
+    class Solution {
+
+        @Test
+        fun solution() {
+            val actual = mergeTwoLists(
+                list1 = ListNode(1, ListNode(2, ListNode(4))),
+                list2 = ListNode(1, ListNode(3, ListNode(4)))
+            )
+
+            expectThat(actual) {
+                isEqualTo(ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(4)))))))
+            }
+
+        }
+
+        fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+
+            var finalList = listMerger(list1, list2)
+            return finalList
+        }
+
+        fun listMerger(listX: ListNode?, listY: ListNode?): ListNode? {
+
+            if (listX == null) {
+                return listY
+            }
+            if (listY == null) {
+                return listX
+            }
+
+            var list1Var = listX
+            var list2Var = listY
+
+
+            var newList: ListNode
+
+            if (list1Var.`val` < list2Var.`val`) {
+                newList = ListNode(list1Var.`val`)
+                list1Var = list1Var.next
+            } else {
+                newList = ListNode(list2Var.`val`)
+                list2Var = list2Var.next
+            }
+                newList.next = listMerger(list1Var, list2Var)
+            return newList
+        }
+    }
+}
