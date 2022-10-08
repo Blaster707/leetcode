9e1e48d6a5dea6755d00776c00d16900e0710d51
Lucas Ferguson <blaster707dc@gmail.com>
2022-10-07 22:31:11 -0500

    First solution of Ex026

---
+++ src/test/kotlin/blaster707/leetcode/Ex026RemoveDuplicatesSortedArray.kt
@@ -0,0 +1,30 @@
+package blaster707.leetcode
+
+import org.junit.jupiter.api.Test
+
+class Ex026RemoveDuplicatesSortedArray {
+
+    class Solution {
+
+        @Test
+        fun solution() {
+            assert(removeDuplicates((intArrayOf(1, 1, 2))) == 2)
+            assert(removeDuplicates(intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)) == 5)
+        }
+
+        fun removeDuplicates(nums: IntArray): Int {
+            var indexInt = 0
+            var indexReplacer = 1
+            while (nums[nums.lastIndex] > nums[indexInt]) {
+                if (nums[indexInt] == nums[indexInt + 1]) {
+                    indexInt++
+                } else {
+                    nums[indexReplacer] = nums[indexInt+1]
+                    indexInt++
+                    indexReplacer++
+                }
+            }
+            return indexReplacer
+        }
+    }
+}
