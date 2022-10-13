80d0fed0e7bdded39bf10c94506cebcec30cf611
Lucas Ferguson <blaster707dc@gmail.com>
2022-10-12 22:07:55 -0500

    Began Ex028

---
+++ src/test/kotlin/blaster707/leetcode/Ex028FindFirstOccurrence.kt
@@ -0,0 +1,18 @@
+package blaster707.leetcode
+
+import org.junit.jupiter.api.Test
+
+class Ex028FindFirstOccurrence {
+
+    class Solution {
+
+        @Test
+        fun solution(){
+
+        }
+        
+        fun strStr(haystack: String, needle: String): Int {
+
+        }
+    }
+}
