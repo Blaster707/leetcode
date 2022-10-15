9f17089bdb541412099ed901567d58aa6979c002
Lucas Ferguson <blaster707dc@gmail.com>
2022-10-15 16:26:49 -0500

    Utilized recursion for solution

--- src/test/kotlin/blaster707/leetcode/Ex012IntegertoRoman.kt
+++ src/test/kotlin/blaster707/leetcode/Ex012IntegertoRoman.kt
@@ -16,27 +16,23 @@ class Ex012IntegertoRoman {
 
         fun intToRoman(num: Int): String {
 
-            var x = num
             var answer = ""
-
-            while (x != 0) {
-                when {
-                    x >= 1000 -> {for (i in 1..(x/1000)) {answer = answer.plus('M')}; x%=1000}
-                    x >= 900 -> {answer = answer.plus("CM"); x-=900}
-                    x >= 500 -> {answer = answer.plus("D"); x-=500}
-                    x >= 400 -> {answer = answer.plus("CD"); x-=400}
-                    x >= 100 -> {for (i in 1..(x/100)) {answer = answer.plus('C')}; x%=100}
-                    x >= 90 -> {answer = answer.plus("XC"); x-=90}
-                    x >= 50 -> {answer = answer.plus("L"); x-=50}
-                    x >= 40 -> {answer = answer.plus("XL"); x-=40}
-                    x >= 10 -> {for (i in 1..(x/10)) {answer = answer.plus('X')}; x%=10}
-                    x >= 9 -> {answer = answer.plus("IX"); x = 0}
-                    x >= 5 -> {answer = answer.plus("V"); x-=5}
-                    x >= 4 -> {answer = answer.plus("IV"); x = 0}
-                    x >= 1 -> {for (i in 1..(x/1)) {answer = answer.plus('I')}; x = 0}
+            when {
+                num >= 1000 -> {for (i in 1..(num / 1000)) {answer = answer.plus('M')}; return answer + intToRoman(num % 1000)}
+                num >= 900 -> {answer = answer.plus("CM"); return answer + intToRoman(num-900)}
+                num >= 500 -> {answer = answer.plus("D"); return answer + intToRoman(num-500)}
+                num >= 400 -> {answer = answer.plus("CD"); return answer + intToRoman(num-400)}
+                num >= 100 -> {for (i in 1..(num/100)) {answer = answer.plus('C')}; return answer + intToRoman(num % 100)}
+                num >= 90 -> {answer = answer.plus("XC"); return answer + intToRoman(num-90)}
+                num >= 50 -> {answer = answer.plus("L"); return answer + intToRoman(num-50)}
+                num >= 40 -> {answer = answer.plus("XL"); return answer + intToRoman(num-40)}
+                num >= 10 -> {for (i in 1..(num/10)) {answer = answer.plus('X')}; return answer + intToRoman(num % 10)}
+                num >= 9 -> {answer = answer.plus("IX"); return answer}
+                num >= 5 -> {answer = answer.plus("V"); return answer + intToRoman(num-5)}
+                num >= 4 -> {answer = answer.plus("IV"); return answer}
+                num >= 1 -> {for (i in 1..(num/1)) {answer = answer.plus('I')}; return answer}
+                else -> return ""
                 }
             }
-            return answer
         }
     }
-}
