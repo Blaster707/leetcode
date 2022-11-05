cbca384a698a6648bb475ee9e2e8d4256f381209
Lucas Ferguson <blaster707dc@gmail.com>
2022-11-05 00:12:54 -0500

    Working solution for Ex005

--- src/test/kotlin/blaster707/leetcode/Ex005LongestPalindromicSubstring.kt
+++ src/test/kotlin/blaster707/leetcode/Ex005LongestPalindromicSubstring.kt
@@ -8,20 +8,32 @@ class Ex005LongestPalindromicSubstring {
 
         @Test
         fun solution() {
-
+            //assert(longestPalindrome("babad") == "bab")
+            assert(longestPalindrome("aacabdkacaa") == "aca")
         }
 
         fun longestPalindrome(s: String): String {
-            var lastPointer = s.length-1
+            var answer = "${s[0]}"
+            var xPointer = 0
             for (x in s) {
-                while (lastPointer != s.indexOf(x)) {
+                var lastPointer = s.length-1
+                if (s.length-1-xPointer < answer.length) {return answer}
+                while (lastPointer != xPointer) {
                     if (s[lastPointer] == x) {
-
+                        val subString = s.substring(xPointer, lastPointer+1)
+                        if (isPalindrome(subString)) {
+                            if (subString.length > answer.length) {
+                                answer = subString
+                                if (s.length-1-xPointer < answer.length) {return answer}
+                                continue
+                            }
+                        }
                     }
+                    if (lastPointer-1 == xPointer) {xPointer++; continue}
                     lastPointer -= 1
                 }
             }
-            return "hi"
+            return answer
         }
 
         fun isPalindrome(x: String): Boolean {
