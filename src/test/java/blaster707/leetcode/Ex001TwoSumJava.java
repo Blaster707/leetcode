1d2b0a864ccb62aa52cab9edf6f09d76a6915b96
Lucas Ferguson <blaster707dc@gmail.com>
2022-11-10 16:21:09 -0600

    Created Ex001TwoSumJava for java practice

---
+++ src/test/java/blaster707/leetcode/Ex001TwoSumJava.java
@@ -0,0 +1,16 @@
+package blaster707.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex001TwoSumJava {
+
+    @Test
+    public void solution() {
+        List<Integer> myList = new ArrayList<>();
+        myList.add(42);
+        assert(myList.size() == 1);
+    }
+}
