d3de0ce1225faf3467922ff9299c4f9405ee0c08
Lucas Ferguson <blaster707dc@gmail.com>
2022-11-20 00:20:19 -0600

    Began putting together some further ideas about classes and how to break it down

--- src/test/kotlin/blaster707/experiments/contactlist/ContactEntry.kt
+++ src/test/kotlin/blaster707/experiments/contactlist/ContactEntry.kt
@@ -2,10 +2,33 @@ package blaster707.experiments.contactlist
 
 data class ContactEntry (
     val person: Person,
-    val mobileNumber: Int?,
-    val homeNumber: Int?,
     val streetAddress: String?,
     val city: String?,
     val state: String?,
-    val zipCode: Int?
-        )
+    val zipCode: Int?){
+
+    val firstName = person.firstName
+    val lastName = person.lastName
+
+
+}
+
+interface PhoneNumber{
+    fun numberType(numberTypeInput: String): String
+}
+
+class MobileNumber(val number: Long): PhoneNumber {
+    val numberType = "Mobile"
+}
+
+data class HomeNumber(val number: Long): PhoneNumber {
+    val numberType = "Home"
+}
+
+data class WorkNumber(val number: Long): PhoneNumber {
+    val numberType = "Work"
+}
+
+data class OtherNumber(val number: Long): PhoneNumber {
+    val numberType = "Other"
+}
--- src/test/kotlin/blaster707/experiments/contactlist/Person.kt
+++ src/test/kotlin/blaster707/experiments/contactlist/Person.kt
@@ -1,7 +1,14 @@
 package blaster707.experiments.contactlist
 
-data class Person (
-    val firstName: String,
-    val lastName: String,
-    val middleName: String?
-        )
+data class Person (val firstName: String, val lastName: String)
+
+class PersonBuilder{
+    fun getPersonInput(): Person{
+        println("What is the first name of the person?")
+        val firstNameInput: String? = readLine()
+        println("What is the last name of the person?")
+        val lastNameInput: String? = readLine()
+        if (firstNameInput == null || lastNameInput == null) {throw Exception("Invalid entry; first name and last name are required.  Cancelling addition of this person.")}
+        else return Person(firstNameInput, lastNameInput)
+    }
+}
